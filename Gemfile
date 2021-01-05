d5f3bea978d9abd2b06525d3a650254febfad828
bill-baggins <billyolebaggins@gmail.com>
2021-01-05 15:15:29 -0500

    removed guardfile

--- Gemfile
+++ Gemfile
@@ -1,8 +1,3 @@
 source 'https://rubygems.org'
 
-group :development do
-  gem 'guard', require: false
-  gem 'guard-bundler', require: false
-end
-
 gem 'gosu', '~> 1.0'
--- gameoflife.rb
+++ gameoflife.rb
@@ -6,9 +6,7 @@
 # 2. Any live cell with two or three live neighbours lives on to the next generation.
 # 3. Any live cell with more than three live neighbours dies, as if by overpopulation.
 # 4. Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
-require 'rubygems'
-require 'bundler/setup'
-Bundler.require(:default)
+require 'gosu'
 
 class Game < Gosu::Window
   def initialize(width, height)

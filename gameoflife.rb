aa4e9b4341ffa7b26d35d5f36cb9b8fa96fcf9eb
bill-baggins <billyolebaggins@gmail.com>
2021-01-05 14:24:46 -0500

    bundler

--- gameoflife.rb
+++ gameoflife.rb
@@ -6,8 +6,10 @@
 # 2. Any live cell with two or three live neighbours lives on to the next generation.
 # 3. Any live cell with more than three live neighbours dies, as if by overpopulation.
 # 4. Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
-require 'gosu'
+require 'rubygems'
 require 'bundler/setup'
+require 'gosu'
+
 
 class Game < Gosu::Window
   def initialize(width, height)
